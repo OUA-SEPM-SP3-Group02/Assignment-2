1a0f25b28d8611a8c2523311c57acdb1e80853a3
karstenbeckqld <s3912792@student.rmit.edu.au>
2022-09-30 09:35:56 +1000

    syncing files

---
+++ src/com/sepm/model/Ticket.java
@@ -0,0 +1,28 @@
+package com.sepm.model;
+
+class Ticket {
+    private int ticketId;
+    private String ticketIssuer;
+    private String ticketEmail;
+    private String ticketLevel;
+    private String ticketStatus;
+
+    Ticket(int id, String issuer, String email, String level, String status) {
+        this.ticketId = id;
+        this.ticketIssuer = issuer;
+        this.ticketEmail = email;
+        this.ticketLevel = level;
+        this.ticketStatus = status;
+    }
+
+    public String toString() {
+        String ticketSummary = "";
+        ticketSummary += this.ticketId + ",";
+        ticketSummary += this.ticketIssuer + ",";
+        ticketSummary += this.ticketEmail + ",";
+        ticketSummary += this.ticketLevel + ",";
+        ticketSummary += this.ticketStatus;
+        return ticketSummary;
+    }
+}
+
