8a38f6661f1e0c2395bbfb42fca4b5e66634224f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-04 08:52:10 +1100

    - Added new commenting to ticket manager main application class. - Registered new AuthenticationController with class.

--- src/com/sepm/core/TicketManager.java
+++ src/com/sepm/core/TicketManager.java
@@ -1,5 +1,6 @@
 package com.sepm.core;
 
+import com.sepm.controller.AuthenticationController;
 import com.sepm.controller.UserController;
 import com.sepm.model.Ticket;
 import com.sepm.service.TicketService;
@@ -11,13 +12,20 @@ public class TicketManager {
     private String activeController;
 
     public TicketManager() {
+        //create our controllers hashmap
         this.controllers = new HashMap<>();
+
+        //register all our controllers with the hashmap
         this.controllers.put("userController", new UserController(this));
+        this.controllers.put("authenticationController", new AuthenticationController(this));
+
+        //set the active controller
         this.activeController = "userController";
 
         //Bind our ticket data to our loaded tickets
         Ticket.bindTicketData(TicketService.loadTicketsFromXMLFile("tickets.xml"));
 
+        //start the input loop
         processInput(new Request());
     }
 
