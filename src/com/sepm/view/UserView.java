3d75c0b0c031f867d8558972726f0db1a45022c5
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-04 08:36:20 +1100

    - Added commenting to user view. - Added foreach loop to show all tickets in response.

--- src/com/sepm/view/UserView.java
+++ src/com/sepm/view/UserView.java
@@ -10,16 +10,27 @@ import java.util.ArrayList;
 public class UserView extends View {
 
     public Request showTickets(Response response) {
+        //SHOW HEADING
         System.out.println("Show tickets.");
+
+        //create our new request
         Request request = new Request();
+
+        //add our user input into the request
         request.add("input", this.getUserInput());
 
-        ArrayList<Ticket> tickets = (ArrayList<Ticket>) response.get("tickets");
-        tickets.forEach(ticket -> {
-            System.out.println(ticket.toString());
-        });
+        //check if the response contains tickets
+        if(response.contains("tickets")) {
 
+            //if so then for each ticket output them
+            for (Ticket ticket : (Ticket[]) response.get("tickets")) {
+                System.out.println(ticket.toString());
+            }
+        }
+
+        //check if the response contains an error
         if (response.contains("error")) {
+            //if so output it
             System.out.println(response.get("error"));
         }
         return request;
