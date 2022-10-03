ef5b295af7037b80542b1883a259ca845d2bf059
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-04 08:30:09 +1100

    - Switched Ticket variables to private and final - Added Static ticket data to ticket model class. - Created BindTicketData method for static data. - Created Static getter to return all the ticket data

--- src/com/sepm/model/Ticket.java
+++ src/com/sepm/model/Ticket.java
@@ -1,13 +1,16 @@
 package com.sepm.model;
 
 public class Ticket {
-    String ticketId;
-    String ticketTitle;
-    String ticketDescription;
-    String ticketIssuer;
-    String ticketEmail;
-    String ticketLevel;
-    String ticketStatus;
+    private final String ticketId;
+    private final String ticketTitle;
+    private final String ticketDescription;
+    private final String ticketIssuer;
+    private final String ticketEmail;
+    private final String ticketLevel;
+    private final String ticketStatus;
+
+    //Private Static Global Array of tickets, is accessed via our getters
+    private static Ticket[] tickets;
 
     public Ticket(String id, String title, String description, String issuer, String email, String level, String status) {
         this.ticketId = id;
@@ -31,6 +34,30 @@ public class Ticket {
         return ticketSummary;
     }
 
-    // Test to see if commits are working
+    //Accepts an array of ticket objects and binds the array to the static private tickets array
+    public static void bindTicketData(Ticket[] tickets){
+        Ticket.tickets = tickets;
+    }
+
+    //----------------------- STATIC GETTERS -----------------------
+    //These methods will provide a way to get ticket models from any
+    //caller in the program.
+
+    //**** GET ALL METHOD ****\\
+    //Returns all the tickets
+    public static Ticket[] getAll(){
+        return Ticket.tickets;
+    }
+
+    //**** GET WERE ISSUER METHOD ****\\
+    //Returns all the tickets that match that issuer
+    public static Ticket[] getWereIssuer(String ticketIssuer){
+        //TODO ADD LOGIC
+        return  null;
+    }
+
+    //@Karsten please feel free to start to add more getters for different criteria!
+    //try and make them generic in the sense that we can reuse them! - Jack Harris
+
 }
 
