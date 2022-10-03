6fc19f0f2f6256f32746672aa7e970b7ba42d490
Morgs61 <morgan.cassar3@gmail.com>
2022-10-03 18:46:51 +1100

    added user,users and inital classes for login

---
+++ src/com/sepm/login/Initial.java
@@ -0,0 +1,49 @@
+package com.sepm.login;
+
+import java.util.Scanner;
+
+public class Initial {
+
+        static Scanner sc = new Scanner(System.in);
+
+        public static void main(String[] args) {
+            String choice;
+            do {
+                // Displays Menu
+                displayMenu();
+                choice = sc.nextLine();
+                // process user's selection
+                // note use of toUpperCase()
+                switch (choice.toUpperCase()) {
+                    case "A":
+                        //loginUser();
+                        break;
+                    case "B":
+                        //forgotPassword();
+                        break;
+                    case "C":
+                        //registerUser();
+                        break;
+                    case "X":
+                        System.out.println("Exiting the program...");
+                        break;
+                    default:
+                        System.out.println("Error - invalid selection!");
+                }
+
+            } while (!choice.equalsIgnoreCase("X"));
+
+        }
+
+        // Displays the Menu
+        public static void displayMenu() {
+            System.out.println("IT Ticketing");
+            System.out.println("");
+            System.out.println("A: Login");
+            System.out.println("B: Forgot Password");
+            System.out.println("C: Register");
+            System.out.println("X: Exit program");
+            System.out.println("");
+            System.out.print("Please enter your selection: ");
+        }
+    }
---
+++ src/com/sepm/login/Users.java
@@ -0,0 +1,11 @@
+package com.sepm.login;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Users {
+
+}
