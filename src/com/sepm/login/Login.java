2bc4e278be99ca8343c403104b895f2e4f00193d
Morgs61 <morgan.cassar3@gmail.com>
2022-10-05 17:14:32 +1100

    added find technician method

--- src/com/sepm/login/Login.java
+++ src/com/sepm/login/Login.java
@@ -1,7 +1,21 @@
 package com.sepm.login;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
+
+
+
 
 public class Login {
 
@@ -9,7 +23,12 @@ public class Login {
 
 
 
-    public static void login(){
+    public static void login() throws IOException, SAXException, ParserConfigurationException {
+        File loginDetails = new File ("serviceDesk.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(loginDetails);
+
         System.out.println("Email: ");
         String email = sc.next();
         System.out.println("Password: ");
@@ -21,7 +40,23 @@ public class Login {
         find(loginuser, email, password);
     }
 
-    public static void find(HashMap<String, String> loginUser, String email, String password) {
+    private static void getAllTechnicians(Document doc)
+    {
+        NodeList staffNodes = doc.getElementsByTagName("serviceDesk");
+        for(int i=0; i<staffNodes.getLength(); i++)
+        {
+            Node staffNode = staffNodes.item(i);
+            if(staffNode.getNodeType() == Node.ELEMENT_NODE)
+            {
+                Element staffElement = (Element) staffNode;
+                String staffId = staffElement.getElementsByTagName("email").item(0).getTextContent();
+                String studentName = staffElement.getElementsByTagName("password").item(0).getTextContent();
+
+            }
+        }
+    }
+
+    public static void find(HashMap<String, String> loginUser, String email, String password) throws IOException, ParserConfigurationException, SAXException {
         for (String i : loginUser.keySet()) {
             if (i.equals(email) && loginUser.get(i).equals(password)) {
                 System.out.println("User is Found!");
@@ -33,6 +68,8 @@ public class Login {
         System.out.println("User Not Found. Login Again");
         login();
     }
+
+
 
 }
 
