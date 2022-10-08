86e96fcd858df35d6cb3fd51784bc65f77d7d1fd
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-08 20:17:58 +1100

    - Added extension for the new parent User class. - Moved static getters and static main model data array to the new parent user class.

--- src/com/sepm/model/ServiceDeskMember.java
+++ src/com/sepm/model/ServiceDeskMember.java
@@ -1,12 +1,8 @@
 package com.sepm.model;
 
-public class ServiceDeskMember {
-    private final String id;
-    private final String name;
-    private final String email;
+public class ServiceDeskMember extends User{
+
     private final String serviceLevel;
-    private final String password;
-    private static ServiceDeskMember[] serviceDeskMembers;
 
     public ServiceDeskMember(String id, String name, String serviceLevel, String email, String password) {
         this.id = id;
@@ -14,28 +10,6 @@ public class ServiceDeskMember {
         this.email = email;
         this.serviceLevel = serviceLevel;
         this.password = password;
-    }
-
-    //Accepts an array of ticket objects and binds the array to the static private tickets array
-    public static void bindServiceDeskData(ServiceDeskMember[] serviceDeskMembers) {
-        ServiceDeskMember.serviceDeskMembers = serviceDeskMembers;
-    }
-
-    //----------------------- STATIC GETTERS -----------------------
-    //These methods will provide a way to get ticket models from any
-    //caller in the program.
-
-    //**** GET ALL METHOD ****\\
-    //Returns all the tickets
-    public static ServiceDeskMember[] getServiceDeskMembers() {
-        return ServiceDeskMember.serviceDeskMembers;
-    }
-
-    //**** GET WERE ISSUER METHOD ****\\
-    //Returns all the tickets that match that issuer
-    public static ServiceDeskMember[] getWereName(String name) {
-        //TODO ADD LOGIC
-        return null;
     }
 
     public String getPassword(){
@@ -49,33 +23,5 @@ public class ServiceDeskMember {
     public String getServiceLevel(){
         return this.serviceLevel;
     }
-
-    public String toString() {
-        String serviceMembersSummary = "";
-        serviceMembersSummary += this.id + ":";
-        serviceMembersSummary += this.name + ":";
-        serviceMembersSummary += this.email + ":";
-        serviceMembersSummary += this.serviceLevel + ":";
-        serviceMembersSummary += this.password + ":";
-        return serviceMembersSummary;
-    }
-
-    public static ServiceDeskMember getServiceDeskMemberByEmail(String email){
-
-        ServiceDeskMember outcome = null;
-
-        for(ServiceDeskMember serviceDeskMember : ServiceDeskMember.serviceDeskMembers){
-            if(serviceDeskMember.email.equals(email)){
-                outcome =  serviceDeskMember;
-            }
-        }
-
-        return outcome;
-    }
-
-
-
-    //@Karsten please feel free to start to add more getters for different criteria!
-    //try and make them generic in the sense that we can reuse them! - Jack Harris
 
 }
