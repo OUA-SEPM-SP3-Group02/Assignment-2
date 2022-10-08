7f00cde3608db6273d1d49373f498114233405b6
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-08 18:29:58 +1100

    - Rebuilt authenticationView -> Welcome to use new standard for application title. - Created AuthenticationView -> Login method to handle the login process.

--- src/com/sepm/view/AuthenticationView.java
+++ src/com/sepm/view/AuthenticationView.java
@@ -1,5 +1,6 @@
 package com.sepm.view;
 
+import com.sepm.core.Ascii;
 import com.sepm.core.Request;
 import com.sepm.core.Response;
 import com.sepm.core.View;
@@ -9,14 +10,48 @@ public class AuthenticationView extends View {
     public Request welcome(Response response){
         Request request = new Request();
 
-        System.out.println("IT Ticketing");
-        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("Service Desk Application (SEPM v0.1)");
+        System.out.println("------------------------------------\n");
+
+        if(response.contains("notification")){
+            System.out.println(Ascii.GREEN+response.get("notification")+Ascii.RESET+"\n");
+        }
+
         System.out.println("A: Login");
         System.out.println("B: Forgot Password");
         System.out.println("C: Register");
-        System.out.println("X: Exit program");
-        System.out.println("");
+        System.out.println("X: Exit program\n");
+
+        if(response.contains("error")){
+            System.out.println(Ascii.RED+response.get("error")+Ascii.RESET+"\n");
+        }
+
         System.out.print("Please enter your selection: ");
+        request.add("input",this.getUserInput());
+
+        return request;
+    }
+
+    public Request login(Response response){
+
+        Request request = new Request();
+
+        System.out.println("------------------------------------");
+        System.out.println("Service Desk Application (SEPM v0.1)");
+        System.out.println("------------------------------------\n");
+        System.out.println("Login:\n");
+
+        if(response.contains("notification")){
+            System.out.println(Ascii.GREEN+response.get("notification")+Ascii.RESET+"\n");
+        }
+
+
+        System.out.println(response.get("header")+" or enter 'X' to cancel and return");
+
+        if(response.contains("error")){
+            System.out.println(Ascii.RED+response.get("error")+Ascii.RESET+"\n");
+        }
 
         request.add("input",this.getUserInput());
 
