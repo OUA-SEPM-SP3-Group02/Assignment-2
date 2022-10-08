df1cbb28d79b06ce312d8b8f56793ccf5da5e8ed
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-08 18:54:53 +1100

    - Added program header to showTicket view method. - Updated notification and error to show with colored text.

--- src/com/sepm/view/UserView.java
+++ src/com/sepm/view/UserView.java
@@ -40,15 +40,17 @@ public class UserView extends View {
     }
 
     public Request showTickets(Response response) {
-        //SHOW HEADING
-        System.out.println("Show tickets.");
-
         //create our new request
         Request request = new Request();
 
+        //SHOW
+        System.out.println("------------------------------------");
+        System.out.println("Service Desk Application (SEPM v0.1)");
+        System.out.println("------------------------------------");
+        System.out.println("Showing tickets for service level "+response.get("service_level")+":\n");
 
-        if (response.contains("Notification")) {
-            System.out.println(response.get("Notification"));
+        if(response.contains("notification")){
+            System.out.println(Ascii.GREEN+response.get("notification")+Ascii.RESET+"\n");
         }
 
         //check if the response contains tickets
@@ -72,10 +74,8 @@ public class UserView extends View {
             }
         }
 
-        //check if the response contains an error
-        if (response.contains("error")) {
-            //if so output it
-            System.out.println(response.get("error"));
+        if(response.contains("error")){
+            System.out.println(Ascii.RED+response.get("error")+Ascii.RESET+"\n");
         }
 
         //add our user input into the request
