4b988800d1ff759cf5abf3c79d1c553a02bdb68b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-15 12:52:59 +1100

    - Added showTicket view method that shows the ticket to the user.

--- src/com/sepm/view/TicketView.java
+++ src/com/sepm/view/TicketView.java
@@ -4,6 +4,7 @@ import com.sepm.core.Ascii;
 import com.sepm.core.Request;
 import com.sepm.core.Response;
 import com.sepm.core.View;
+import com.sepm.model.Ticket;
 
 public class TicketView extends View {
 
@@ -35,5 +36,30 @@ public class TicketView extends View {
 
         return request;
 
+    }
+
+    public Request showTicket(Response response){
+        Request request = new Request();
+
+        Ticket ticket = (Ticket) response.get("ticket");
+
+        System.out.println("------------------------------------");
+        System.out.println("Service Desk Application (SEPM v0.1)");
+        System.out.println("------------------------------------");
+        System.out.println("Open Ticket "+ticket.getTicketId()+":\n");
+
+        System.out.println("Title: "+ticket.getTicketTitle());
+        System.out.println("Description: "+ticket.getTicketDescription());
+        System.out.println("Level: "+ticket.getTicketLevel());
+        System.out.println("Status: "+ ticket.getTicketStatus()+"\n");
+
+        if (response.contains("error")) {
+            System.out.println(Ascii.RED + response.get("error") + Ascii.RESET + "\n");
+        }
+
+        System.out.println("Press 'X' to return or press 'A' to update the status (switches between closed & open)");
+        request.add("input", this.getUserInput());
+
+        return request;
     }
 }
