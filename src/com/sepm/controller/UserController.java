6c8d0c4087d9adf2a77bfd49ff2a993bd221b215
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-15 12:53:50 +1100

    - Added selectTicket() logic method to the user controller, this validates the user has selected a valid ticket. - Switched mainMenu selection 'A' to now direct the user to the ticket selection view.

--- src/com/sepm/controller/UserController.java
+++ src/com/sepm/controller/UserController.java
@@ -4,8 +4,8 @@ import com.sepm.core.Controller;
 import com.sepm.core.Request;
 import com.sepm.core.Response;
 import com.sepm.core.Application;
-import com.sepm.model.ServiceDeskMember;
 import com.sepm.model.Ticket;
+import com.sepm.view.TicketView;
 import com.sepm.view.UserView;
 
 public class UserController extends Controller {
@@ -20,8 +20,8 @@ public class UserController extends Controller {
         Request request = new Request();
         switch(this.activeSubView) {
             case "mainMenu" -> request =  ((UserView)this.view).mainMenu(response);
-            case "showTickets" -> request = ((UserView)this.view).showTickets(response);
             case "showServiceMembers" -> request = ((UserView)this.view).showServiceDeskMembers(response);
+            case "selectTicket" -> request = ((UserView)this.view).selectTicket(response);
         }
 
         this.app.processInput(request);
@@ -34,8 +34,8 @@ public class UserController extends Controller {
 
         switch (this.activeSubView) {
             case "mainMenu" -> response = this.mainMenu(request);
-            case "showTickets" -> response = this.showTickets(request);
             case "showServiceMembers" -> response = this.showServiceMembers(request);
+            case "selectTicket" -> response = this.selectTicket(request);
         }
 
         this.app.updateView(response);
@@ -43,38 +43,45 @@ public class UserController extends Controller {
 
     private Response mainMenu(Request request){
         Response response = new Response();
+        response.add("tickets", Ticket.getWhereName(this.app.getUser().getName()));
+
 
         switch (request.get("input").toString()) {
-            case "A" -> {
-                this.activeSubView = "showTickets";
-                response.add("user", this.app.getUser());
-                response.add("tickets", Ticket.getWereLevel(this.app.getUser().getServiceLevel()));
-            }
+            case "A" -> this.activeSubView = "selectTicket";
             case "B", "C", "D" -> response.add("error","Feature not yet added");
-            case "E" -> {this.app.setActiveController("ticketController"); this.app.processInput(new Request());}
-            case "F" -> {this.app.setActiveController("ticketController"); this.app.processInput(new Request());}
+            case "E", "F" -> {this.app.setActiveController("ticketController"); this.app.processInput(new Request());}
             case "X" -> {this.app.setActiveController("authenticationController"); this.app.setActiveSubView("welcome"); response.add("notification","User successfully logged out"); this.app.setUser(null);}
+
             default -> response.add("error", "Invalid input, please select A, B, C, D, E or X!");
         }
 
         return response;
     }
 
-    private Response showTickets(Request request){
+    private Response selectTicket(Request request){
         Response response = new Response();
 
-        if(request.containsUserInput()){
-            if(request.get("input").equals("X")){
-                this.activeSubView = "mainMenu";
-                response.add("notification","Returned "+this.app.getUser().getName()+" to the main menu from show tickets menu");
-                return response;
-            }
+        if(request.containsUserInput() && request.get("input").toString().equals("X")){
+            this.activeSubView = "mainMenu";
+            response.add("tickets", Ticket.getWhereName(this.app.getUser().getName()));
+            return response;
         }
 
-        //response.add("service_level",this.app.getUser().getServiceLevel());
-        //response.add("tickets", Ticket.getWereLevel(this.app.getUser().getServiceLevel()));
-        response.add("tickets", Ticket.getWhereName(this.app.getUser().getName()));
-        response.add("user", this.app.getUser());
+        if(!request.containsUserInput() || !request.isInteger()){
+            response.add("error","Please enter a valid ticket ID as a integer");
+            return response;
+        }
+
+        if(!this.app.getUser().validateTicketSelectionForUser(Integer.parseInt(request.get("input").toString()))){
+            response.add("error","Ticket not found for ID '"+request.get("input").toString()+"'");
+            return response;
+        }
+
+        this.app.setActiveController("ticketController");
+        this.app.setActiveSubView("showTicket");
+        ((TicketController)this.app.getActiveController()).selectedTicketId = Integer.parseInt(request.get("input").toString());
+        request.resetUserInput();
+        this.app.processInput(request);
 
         return response;
     }
@@ -83,5 +90,6 @@ public class UserController extends Controller {
         Response response = new Response();
         return response;
     }
+
 
 }
